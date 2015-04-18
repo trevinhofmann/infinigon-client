ab8c2c6fc4851494f7fa7d00e9abcd2f1fb8e2cb
Trevin Hofmann <trevinhofmann@gmail.com>
2015-04-18 11:04:13 -0500

    Use mouse

--- js/index.js
+++ js/index.js
@@ -16,4 +16,38 @@ $(document).ready(function(){
     }, 15);
   });
 
+  mouse = new Mouse();
+
+  mouse.onLeftDown(function(){
+    if (!mouse.leftDownInterval) {
+      mouse.leftDownInterval = setInterval(function(){
+        var offset = board.div.offset();
+        var x = mouse.x - offset.left;
+        var y = offset.top + board.size.height - mouse.y;
+        api.sendLeftClick(x, y);
+      }, 40);
+    }
+  });
+
+  mouse.onRightDown(function(){
+    if (!mouse.rightDownInterval) {
+      mouse.rightDownInterval = setInterval(function() {
+        var offset = board.div.offset();
+        var x = mouse.x - offset.left;
+        var y = offset.top + board.size.height - mouse.y;
+        api.sendRightClick(x, y);
+      }, 40);
+    }
+  });
+
+  mouse.onLeftUp(function(){
+    clearInterval(mouse.leftDownInterval);
+    mouse.leftDownInterval = false;
+  });
+
+  mouse.onRightUp(function(){
+    clearInterval(mouse.rightDownInterval);
+    mouse.rightDownInterval = false;
+  });
+
 });
