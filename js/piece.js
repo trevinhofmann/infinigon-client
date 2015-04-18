b3cb81394dd2c9d970b442f3b154c36d7887b568
Trevin Hofmann <trevinhofmann@gmail.com>
2015-04-18 11:16:12 -0500

    Piece.update

--- js/piece.js
+++ js/piece.js
@@ -62,3 +62,25 @@ function Piece(id, options) {
   }
 
 }
+
+Piece.prototype.update = function(board) {
+  var movementX = this.target.x - this.position.x;
+  var movementY = this.target.y - this.position.y;
+  if (movementX != 0 || movementY != 0) {
+    var factor = this.speed / Math.sqrt(Math.pow(movementX, 2) + Math.pow(movementY, 2));
+    if (factor < 1) {
+      movementX *= factor;
+      movementY *= factor;
+    }
+  }
+  var x = this.position.x + movementX;
+  x = Math.min(x, this.board.size.width - this.size/2);
+  x = Math.max(x, this.size/2);
+  this.position.x = x;
+  var y = this.position.y + movementY;
+  y = Math.min(y, this.board.size.height - this.size/2);
+  y = Math.max(y, this.size/2);
+  this.position.y = y;
+  this.div.css('left', (this.position.x - this.size/2)+'px');
+  this.div.css('bottom', (this.position.y - this.size/2)+'px');
+};
